362ff89c6cfb8c329768096a8159126479a29b35
ChefBoStudies <98541256+ChefBoStudies@users.noreply.github.com>
2025-09-13 02:21:26 +0800

    chore: default event slug to 'lensa-kita'

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ window.__USE_REAL_API__ = true;
 
 // Default route if none provided: /e/:slug
 if (!location.pathname.startsWith('/e/')) {
-  const slug = 'lensa-kami';
+  const slug = 'lensa-kita';
   history.replaceState({}, '', `/e/${slug}`);
 }
