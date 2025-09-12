f84e261bf2dba45160589b5bf9f5324730168d7a
ChefBoStudies <98541256+ChefBoStudies@users.noreply.github.com>
2025-09-13 02:15:32 +0800

    chore: rename header to 'Lensa Kita'

--- src/ui/components/header.js
+++ src/ui/components/header.js
@@ -3,7 +3,7 @@ import { el } from '../../lib/dom.js';
 export function Header({ title }) {
   const root = el('header', { class: 'header' }, [
     el('div', { class: 'header-inner container' }, [
-      el('div', { class: 'header-title' }, 'Lensa Kami')
+      el('div', { class: 'header-title' }, 'Lensa Kita')
     ])
   ]);
   return root;
